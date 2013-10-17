87c5bcffa574831a2a2f2713b9502315d2a20de4
John Simone <john@heroku.com>
2013-10-17 15:50:32 -0700

    add cloudconnect access

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,9 +1,14 @@
 package com.example;
 
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
@@ -12,10 +17,46 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    URI dbUri = new URI(System.getenv("CLOUDCONNECT_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
+        
+        Connection conn = null;
+        try {
+        	conn = getConnection();
+        	Statement stmt = conn.createStatement();
+        	ResultSet rs = stmt.executeQuery("select name from account");
+        	while(rs.next()) {
+        		String name = rs.getString("name");
+        		out.write(name.getBytes());
+        	}
+        } catch (SQLException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } catch (URISyntaxException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+        } finally {
+        	if(conn != null) {        		
+        		try {
+	                conn.close();
+                } catch (SQLException e) {
+	                // TODO Auto-generated catch block
+	                e.printStackTrace();
+                }
+        	}
+        }
         
         out.write("Hello Heroku".getBytes());
         out.flush();
